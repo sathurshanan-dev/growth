44c1b3b10ef2dc8c381f4d78a55f3ecfdd2765dd
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2026-01-04 16:56:24 +1100

    update formatting

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 // FAQ Accordion
 document.addEventListener('DOMContentLoaded', () => {
-  const faqContainer = document.querySelector('.faq-content');
+  const faq_container = document.querySelector('.faq-content');
 
-  faqContainer.addEventListener('click', (e) => {
-    const groupHeader = e.target.closest('.faq-group-header');
+  faq_container.addEventListener('click', (e) => {
+    const group_header = e.target.closest('.faq-group-header');
 
-    if (!groupHeader) return;
+    if (!group_header) return;
 
-    const group = groupHeader.parentElement;
-    const groupBody = group.querySelector('.faq-group-body');
-    const icon = groupHeader.querySelector('i');
+    const group = group_header.parentElement;
+    const group_body = group.querySelector('.faq-group-body');
+    const icon = group_header.querySelector('i');
 
     // Toggle icon
     icon.classList.toggle('fa-plus');
     icon.classList.toggle('fa-minus');
 
     // Toggle visibility of body
-    groupBody.classList.toggle('open');
+    group_body.classList.toggle('open');
 
     // Close other open FAQ bodies
-    const otherGroups = faqContainer.querySelectorAll('.faq-group');
+    const other_groups = faq_container.querySelectorAll('.faq-group');
 
-    otherGroups.forEach((otherGroup) => {
-      if (otherGroup !== group) {
-        const otherGroupBody = otherGroup.querySelector('.faq-group-body');
-        const otherIcon = otherGroup.querySelector('.faq-group-header i');
+    other_groups.forEach((other_group) => {
+      if (other_group !== group) {
+        const otherGroupBody = other_group.querySelector('.faq-group-body');
+        const otherIcon = other_group.querySelector('.faq-group-header i');
 
         otherGroupBody.classList.remove('open');
         otherIcon.classList.remove('fa-minus');
@@ -36,10 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Mobile Menu
 document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerButton = document.querySelector('.hamburger-button');
-  const mobileMenu = document.querySelector('.mobile-menu');
+  const hamburger_button = document.querySelector('.hamburger-button');
+  const mobile_menu = document.querySelector('.mobile-menu');
 
-  hamburgerButton.addEventListener('click', () =>
-    mobileMenu.classList.toggle('active')
+  hamburger_button.addEventListener('click', () =>
+    mobile_menu.classList.toggle('active')
   );
 });
